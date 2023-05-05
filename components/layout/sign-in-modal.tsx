4db5a374d1a05f59f42a19589f2cc8f85fd09872
SheldenShi <shelden@berkeley.edu>
2023-05-05 12:30:10 -0700

    modify texts

--- components/layout/sign-in-modal.tsx
+++ components/layout/sign-in-modal.tsx
@@ -21,9 +21,9 @@ const SignInModal = ({
 
   return (
     <Modal showModal={showSignInModal} setShowModal={setShowSignInModal}>
-      <div className="w-full overflow-hidden shadow-xl md:max-w-md md:rounded-2xl md:border md:border-gray-200">
+      <div className="w-full overflow-hidden shadow-xl md:max-w-2xl md:rounded-2xl md:border md:border-gray-200">
         <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 bg-white px-4 py-6 pt-8 text-center md:px-16">
-          <a href="https://precedent.dev">
+          {/* <a href="https://precedent.dev">
             <Image
               src="/logo.png"
               alt="Logo"
@@ -31,8 +31,8 @@ const SignInModal = ({
               width={20}
               height={20}
             />
-          </a>
-          <h1 className="font-display text-2xl font-bold">Log in to continue.</h1>
+          </a> */}
+          <h1 className="font-display text-3xl font-bold">Log in to continue.</h1>
           <h2 className="text-md font-display font-bold">
             Don&apos;t have an account?
           </h2>
@@ -60,6 +60,12 @@ const SignInModal = ({
               </>
             )}
           </button>
+        </div>
+        <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 bg-white px-4 py-6 pt-8 text-center md:px-16">
+          <h2 className="text-md font-display font-bold">
+            Don&apos;t have an account?
+          </h2>
+          <p className="text-sm">If you have an invitation code, click here to create your account, otherwise click here to apply for an account.</p>
         </div>
       </div>
     </Modal>
