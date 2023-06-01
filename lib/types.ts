eca0e882b80a1263effeeec3b42121d7f214299a
SheldenShi <shelden@berkeley.edu>
2023-06-01 15:48:45 -0700

    display product

--- lib/types.ts
+++ lib/types.ts
@@ -1,30 +1,30 @@
 export type UserInfo = {
-    id: string;
-    email: string;
-    firstName: string;
-    lastName: string;
-    bio: string;
-    image: string;
-    tk: string;
-    ig: string;
+  id: string
+  email: string
+  firstName: string
+  lastName: string
+  bio: string
+  image: string
+  tk: string
+  ig: string
 }
 
-
 export interface AffiliateLink {
-    id: string;
-    url: string;
-    createdAt: Date;
-    product: Product;
-    clicks: number;
-    orders: number;
-    earned: number;
-    content?: string;
+  id: string
+  url: string
+  createdAt: Date
+  product: Product
+  clicks: number
+  orders: number
+  earned: number
+  content?: string
 }
 
 export interface Product {
-    tittle: string;
-    description: string;
-    link: string;
-    commission: number;
-    image: string;
+  tittle: string
+  description: string
+  link: string
+  commission: number
+  image: string
+  brand: string
 }
