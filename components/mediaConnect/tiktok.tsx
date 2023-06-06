5b83f50ecac12ce87aa99e8a8ffd797844a636da
Tony Kam <91111730+kamikariat@users.noreply.github.com>
2023-06-05 22:21:55 -0700

    basic UI Mockup

---
+++ components/mediaConnect/tiktok.tsx
@@ -0,0 +1,10 @@
+
+'use client'
+
+export default function TikTokConnect() {
+    return (
+        <button disabled className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Connect your TikTok
+        </button>
+      )
+}
