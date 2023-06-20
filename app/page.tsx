e400ff31fe08d26d97e61bd111295c8abcc5d7a7
Tony Kam <91111730+kamikariat@users.noreply.github.com>
2023-06-19 17:59:58 -0700

    Updated copywriting for landing page

--- app/page.tsx
+++ app/page.tsx
@@ -37,16 +37,14 @@ export default async function Home() {
                 className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm md:text-7xl md:leading-[5rem]"
                 style={{ animationDelay: '0.15s', animationFillMode: 'forwards' }}
               >
-                <Balancer>Where elite creators and brands collaborate</Balancer>
+                <Balancer> Recommend Products, Earn Commission.</Balancer>
               </h1>
               <p
                 className="mt-6 animate-fade-up text-center text-gray-500 opacity-0 md:text-xl"
                 style={{ animationDelay: '0.25s', animationFillMode: 'forwards' }}
               >
                 <Balancer>
-                  Join the world’s leading brands and content creators to access
-                  powerful tools and your dream collaborations on the #1 platform for
-                  product promotion on social.
+                  Unlock hundreds of affiliate programs. Earn commission on every sale. Track analytics for all of your links.  
                 </Balancer>
               </p>
               <div
