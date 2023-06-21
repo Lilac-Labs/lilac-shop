d8edae7c5b936329c514da4bb028b6e0c811d218
Tony Kam <91111730+kamikariat@users.noreply.github.com>
2023-06-20 20:41:28 -0700

    Update logo to lilac.png

--- app/page.tsx
+++ app/page.tsx
@@ -71,7 +71,7 @@ export default async function Home() {
                       strokeLinejoin="round"
                     />
                   </svg>
-                  <p>Apply for waitlist</p>
+                  <p>Apply</p>
                 </a>
                 <a
                   className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-5 py-2 text-sm text-gray-600 shadow-md transition-colors hover:border-gray-800"
@@ -81,7 +81,7 @@ export default async function Home() {
                 >
                   <Github />
                   <p>
-                    <span className="hidden sm:inline-block">Star on</span> GitHub{' '}
+                    <span className="hidden sm:inline-block">Contact </span> Us{' '}
                     <span className="font-semibold">{nFormatter(stars)}</span>
                   </p>
                 </a>
--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -32,13 +32,13 @@ export default function NavBar({ session }: { session: Session | null }) {
         <div className="mx-5 flex h-16 max-w-screen-xl items-center justify-between xl:mx-auto">
           <Link href="/" className="flex items-center font-display text-3xl">
             <Image
-              src="/logo.png"
+              src="/lilac.png"
               alt="Precedent logo"
-              width="30"
-              height="30"
+              width="50"
+              height="50"
               className="mr-2 rounded-sm"
             ></Image>
-            <p>myStore</p>
+            <p>Lilac</p>
           </Link>
 
           {session && !isUserInfo && (
