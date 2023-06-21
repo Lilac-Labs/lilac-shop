206539e4320739e034c34f12f0acaa7c3d1bde35
Tony Kam <91111730+kamikariat@users.noreply.github.com>
2023-06-21 06:28:40 -0700

    fix user profile different size

--- components/home/usercard.tsx
+++ components/home/usercard.tsx
@@ -11,8 +11,19 @@ export default function ProductDemo({pfpPath, shoplink, product1img, product1lin
 return (
     <div className="w-7/24 mx-5">
         <div className="w-full h-14/24">
-            <Image className="flex flex-auto object-cover w-full h-full rounded-3xl" src={pfpPath} width={200} height={200} alt="pfp-image" />
+            <img className="object-cover
+                            flex
+                            flex-auto
+                            w-full
+                            max-w-64
+                            max-h-64
+                            h-full
+                            rounded-3xl"
+                src={pfpPath}
+                alt="pfp-image" />
         </div>
+
+
         <div className='w-full flex flex-row justify-evenly -mt-6'>
             <Link href="https://www.bloomingdales.com/shop/product/the-mens-store-at-bloomingdales-cashmere-v-neck-sweater-100-exclusive?ID=3418434&pla_country=US&cm_mmc=Google-PLA-ADC-_-GMM3+-+Mens+-+All+Other+Mens+Categories-_-Mens+Formal+Wear-_-190089875412USA-_-go_cmp-13048465671_adg-123044759238_ad-520733021264_pla-1261067851097_dev-c_ext-_prd-190089875412USA&gad=1&gclid=Cj0KCQjw1rqkBhCTARIsAAHz7K3y7_XUdneK8Us4vRJ15GGoNhm4L7n10nhiMYYodCg7DhzQSxC0RD8aAl5fEALw_wcB">
                 <Image className="rounded-lg shadow-lg shadow-grew" src={product1img} width={100} height={100} alt="product-1"/>
