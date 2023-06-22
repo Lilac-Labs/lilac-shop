6d43a10975ec143e8eacc77931f91912d6530c3b
Tony Kam <91111730+kamikariat@users.noreply.github.com>
2023-06-21 20:56:26 -0700

    fixed dynamic links on demo cards

--- components/home/usercard.tsx
+++ components/home/usercard.tsx
@@ -26,17 +26,17 @@ return (
 
 
         <div className='w-full flex flex-row justify-evenly -mt-6'>
-            <Link href="https://www.bloomingdales.com/shop/product/the-mens-store-at-bloomingdales-cashmere-v-neck-sweater-100-exclusive?ID=3418434&pla_country=US&cm_mmc=Google-PLA-ADC-_-GMM3+-+Mens+-+All+Other+Mens+Categories-_-Mens+Formal+Wear-_-190089875412USA-_-go_cmp-13048465671_adg-123044759238_ad-520733021264_pla-1261067851097_dev-c_ext-_prd-190089875412USA&gad=1&gclid=Cj0KCQjw1rqkBhCTARIsAAHz7K3y7_XUdneK8Us4vRJ15GGoNhm4L7n10nhiMYYodCg7DhzQSxC0RD8aAl5fEALw_wcB">
+            <Link href={product1link}>
                 <Image className="rounded-lg shadow-lg shadow-grew" src={product1img} width={100} height={100} alt="product-1"/>
             </Link>
             
-            <Link href="https://www.alanpaine-usa.com/products/wardlow-cashmere-check-scarf-in-pitch-green">
-                <Image className="rounded-lg shadow-lg shadow-grew" src="/tk-product-2.png" width={100} height={100} alt="product-2" />
+            <Link href={product2link}>
+                <Image className="rounded-lg shadow-lg shadow-grew" src={product2img} width={100} height={100} alt="product-2" />
             </Link>
         </div>
         <div className="flex flex-row justify-center">
         <Button className="flex w-3/6 mt-3">
-            <Link href="/tonykam">
+            <Link href={shoplink}>
                 <p>Visit shop</p>
             </Link>
         </Button>
