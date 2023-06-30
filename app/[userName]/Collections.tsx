90cb2fc02ea4b048e00e66792fd69b6c4de286d3
SheldenShi <shelden@berkeley.edu>
2023-06-29 21:47:18 -0700

    functional version

--- app/[userName]/Collections.tsx
+++ app/[userName]/Collections.tsx
@@ -12,17 +12,32 @@ import Image from 'next/image'
 import CollectionDropdown from './collection-dropdown'
 import { Collection } from '@/lib/types'
 import useWindowSize from '@/lib/hooks/use-window-size'
+import {
+  DragDropContext,
+  Droppable,
+  Draggable,
+  DropResult,
+  DraggableProvided,
+  DraggableProvidedDragHandleProps,
+} from 'react-beautiful-dnd'
+import { Hand, Move } from 'lucide-react'
 
 export default function Collections({ userName }: { userName: string }) {
   const { userInfo } = useUserInfoContext()
   const { collections: ownerCollections, setCollections: setOwnerCollections } =
     useAffiliateLinksContext()
-  const isOwner = userInfo.userProfile?.userName === userName
   const [collections, setCollections] = useState<Collection[]>(
     ownerCollections as Collection[],
   )
+
+  const isOwner = userInfo.userProfile?.userName === userName
+  const [isMounted, setIsMounted] = useState(false)
   const [collectiondUpdated, setCollectionUpdated] = useState(false)
   const router = useRouter()
+
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
 
   useEffect(() => {
     // get user's collections
@@ -63,110 +78,73 @@ export default function Collections({ userName }: { userName: string }) {
 
   // drag and drop
 
-  // const ondragend = (result: any) => {
-  //   if (!result.destination) {
-  //     return
-  //   }
-  //   if (result.destination.index === result.source.index) {
-  //     return
-  //   }
+  const onDragEnd = (result: DropResult) => {
+    const updateCollectionOrder = async (newOrder: number[]) => {
+      const res = await fetcher(
+        `/api/users/by-uuid/${userInfo.id}/collections/collectionsOrder`,
+        {
+          method: 'PATCH',
+          body: JSON.stringify({
+            newOrder: newOrder,
+          }),
+        },
+      )
+    }
 
-  //   const newCollections = reorder(
-  //     collections,
-  //     result.source.index,
-  //     result.destination.index,
-  //   )
-  //   setCollections(newCollections)
-  // }
+    // console.log('drag end', result)
+    if (!result.destination) {
+      return
+    }
+    if (result.destination.index === result.source.index) {
+      return
+    }
+
+    const newCollections = reorder(
+      collections,
+      result.source.index,
+      result.destination.index,
+    )
+
+    updateCollectionOrder(newCollections.map((collection) => collection.id))
+    setCollections(newCollections)
+  }
 
   return (
     <div className="mt-12 flex flex-col">
-      {collections.map((collection) => {
-        return (
-          (isOwner || collection.affiliateLinks.length > 0) && (
-            <div className="py-6" key={collection.id}>
-              <Link href={`/collections/${collection.id}`}>
-                <div className="border-round border-b-2 border-gray-300 px-5">
-                  {isOwner && collection.affiliateLinks.length === 0 ? (
-                    <div className="jusify-between flex flex-row">
-                      <div className="">
-                        <Image
-                          src="/addNew.jpg"
-                          alt="Empty collection"
-                          width={175}
-                          height={175}
-                        />
-                      </div>
-                      {Array.from(
-                        Array(isSm ? 1 : isMobile ? 2 : 3).keys(),
-                      ).map((idx) => {
-                        return (
-                          <div
-                            className="w-[175px]"
-                            key={collection.id + idx}
-                          ></div>
-                        )
-                      })}
-                    </div>
-                  ) : (
-                    <div className="flex flex-row justify-between">
-                      {collection.affiliateLinks
-                        .slice(0, isSm ? 2 : isMobile ? 3 : 4)
-                        .map((affiliateLink) => {
-                          return (
-                            <div className={''} key={affiliateLink.id}>
-                              <Image
-                                src={affiliateLink.image}
-                                alt={affiliateLink.title}
-                                width={175}
-                                height={175}
-                              />
-                            </div>
-                          )
-                        })}
-                      {collection.affiliateLinks.length <
-                        (isSm ? 2 : isMobile ? 3 : 4) &&
-                        Array.from(
-                          Array(
-                            (isSm ? 2 : isMobile ? 3 : 4) -
-                              collection.affiliateLinks.length,
-                          ).keys(),
-                        ).map((idx) => {
-                          return (
-                            <div
-                              className="w-[175px]"
-                              key={collection.id + 'empty' + idx}
-                            ></div>
-                          )
-                        })}
-                    </div>
-                  )}
-                </div>
+      <DragDropContext onDragEnd={onDragEnd}>
+        {isMounted && (
+          <Droppable droppableId="collections">
+            {(provided) => (
+              <div {...provided.droppableProps} ref={provided.innerRef}>
+                {collections.map((collection, index) => {
+                  return (
+                    <Draggable
+                      key={collection.id}
+                      draggableId={String(collection.id)}
+                      index={index}
+                    >
+                      {(provided) => (
+                        <div
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                        >
+                          <CollectionElement
+                            collection={collection}
+                            isOwner={isOwner}
+                            dragHandleProps={provided.dragHandleProps}
+                          />
+                        </div>
+                      )}
+                    </Draggable>
+                  )
+                })}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        )}
+      </DragDropContext>
 
-                <div className="flex flex-col px-5">
-                  <div className="flex flex-row justify-between ">
-                    <p className="mt-3 text-lg font-bold">{collection.title}</p>
-                    {isOwner && (
-                      <div
-                        onClick={(event) => {
-                          event.preventDefault()
-                        }}
-                      >
-                        <CollectionDropdown
-                          collectionId={collection.id}
-                        ></CollectionDropdown>
-                      </div>
-                    )}
-                  </div>
-                  <p className="text-gray-500">
-                    {collection.affiliateLinks.length} products
-                  </p>
-                </div>
-              </Link>
-            </div>
-          )
-        )
-      })}
       {isOwner && (
         <div className="flex flex-row justify-center">
           <Button className="w-48" onClick={addCollectionOnClick}>
@@ -175,6 +153,106 @@ export default function Collections({ userName }: { userName: string }) {
           </Button>
         </div>
       )}
+    </div>
+  )
+}
+
+function CollectionElement({
+  collection,
+  isOwner,
+  dragHandleProps,
+}: {
+  collection: Collection
+  isOwner: boolean
+  dragHandleProps: DraggableProvidedDragHandleProps | undefined | null
+}) {
+  const { isSm, isMobile, isLg } = useWindowSize()
+  console.log(dragHandleProps, 'provided')
+  return (
+    <div className="py-6" key={collection.id}>
+      <Link href={`/collections/${collection.id}`}>
+        <div className="border-round border-b-2 border-gray-300 px-5">
+          {
+            <div className="flex flex-row justify-between">
+              {isOwner && collection.affiliateLinks.length === 0 && (
+                <div className="">
+                  <Image
+                    src="/addNew.jpg"
+                    alt="Empty collection"
+                    width={175}
+                    height={175}
+                  />
+                </div>
+              )}
+              {collection.affiliateLinks
+                .slice(0, isSm ? 2 : isMobile ? 3 : 4)
+                .map((affiliateLink) => {
+                  return (
+                    <div className={''} key={affiliateLink.id}>
+                      <Image
+                        src={affiliateLink.image}
+                        alt={affiliateLink.title}
+                        width={175}
+                        height={175}
+                      />
+                    </div>
+                  )
+                })}
+              {collection.affiliateLinks.length <
+                (isSm ? 2 : isMobile ? 3 : 4) &&
+                Array.from(
+                  Array(
+                    (isSm ? 2 : isMobile ? 3 : 4) -
+                      collection.affiliateLinks.length ===
+                      0
+                      ? 1
+                      : collection.affiliateLinks.length,
+                  ).keys(),
+                ).map((idx) => {
+                  return (
+                    <div
+                      className="w-[175px]"
+                      key={collection.id + 'empty' + idx}
+                    ></div>
+                  )
+                })}
+            </div>
+          }
+        </div>
+
+        <div className="flex flex-col px-5">
+          <div className="flex flex-row justify-between ">
+            <p className="mt-3 text-lg font-bold">{collection.title}</p>
+            {isOwner && (
+              <div className="flex flex-row space-x-5">
+                <div
+                  {...dragHandleProps}
+                  onClick={(event) => {
+                    event.preventDefault()
+                  }}
+                  className="flex items-center justify-center"
+                >
+                  <Move className="h-6 w-6" />
+                </div>
+
+                <div
+                  onClick={(event) => {
+                    event.preventDefault()
+                  }}
+                  className="flex items-center justify-center"
+                >
+                  <CollectionDropdown
+                    collectionId={collection.id}
+                  ></CollectionDropdown>
+                </div>
+              </div>
+            )}
+          </div>
+          <p className="text-gray-500">
+            {collection.affiliateLinks.length} products
+          </p>
+        </div>
+      </Link>
     </div>
   )
 }
